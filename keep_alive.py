8a79fc040d85ccef7dd26b02ae57e7a75f956b3a
Edder06 <50505673+Edder06@users.noreply.github.com>
2024-11-21 07:41:52 -0500

    Update keep_alive to restart option

--- keep_alive.py
+++ keep_alive.py
@@ -4,22 +4,39 @@ import time
 from threading import Thread
 
 app = Flask(__name__)
+t1 = None
+restart = False
 
 @app.route('/')
 def index():
-    return "Alive"
+  return "Alive"
 
 def run():
-  app.run(host='0.0.0.0', port=8080)
+  global restart
+  while True:
+    app.run(host='0.0.0.0', port=8080)
+
+    if restart:
+      print("reiniciando", flush= True)
+      break
 
 def keep_alive():  
-    t = Thread(target=run)
-    t.start()
+  global t1
+  global restart
+  t1 = Thread(target=run)
+  t1.start()
 
-def restart():
-    os.execl(sys.executable, sys.executable, *sys.argv)
+# def restart():
+#   os.execl(sys.executable, sys.executable, *sys.argv)
 
 def keep_restart():
-   #restart thread
-    t = Thread(target=restart)
-    t.start()
+  #restart thread
+  global t1
+  global restart
+  time.sleep(1)
+  print("volvi", flush=True)
+  restart = True
+  t1.join(2)
+  # t1.isAlive()
+  print(t1.is_alive(), flush=True)
+  print("termine", flush=True)
